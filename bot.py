f671b956150d1806a284eb1c9f4a804eaa44aea1
Мусатова Татьяна <MusatovaTF@yandex.ru>
2024-03-28 09:23:53 +0300

    корректировка отправки сообщений

--- bot.py
+++ bot.py
@@ -14,9 +14,9 @@ from bot_app.handlers.base_commands import base_commands_router
 from bot_app.handlers.callbacks_handler import callback_router
 
 from bot_app.handlers.user_registration import user_reg_router
-from bot_app.mailing.mailing import meeting_mailing, meeting_reminder_mailing
+from bot_app.mailing.mailing import (meeting_reminder_mailing,
+                                     newsletter_about_the_meeting)
 from bot_app.middleware.dp import DataBaseSession
-from bot_app.mailing.distribution import distribution
 
 
 async def on_startup():
@@ -50,20 +50,20 @@ async def main() -> None:
 
     sql_session = await anext(get_async_session())
 # ДЛЯ ТЕСТИРОВАНИЯ РАССЫЛКИ НА ПН НУЖНО РАЗКОММЕНТИРОВАТЬ СТРОКИ 42-43, РАССЫЛКА БУДЕТ ПРОИСХОДИТЬ ПРИ ЗАПУСКЕ БОТА
-    # scheduler.add_job(distribution, args=(sql_session,),
-    #                   next_run_time=datetime.now())
+    scheduler.add_job(newsletter_about_the_meeting, args=(sql_session,),
+                      next_run_time=datetime.now())
     # scheduler.add_job(distribution, args=(sql_session,),
     #                   trigger='cron', day_of_week='thu', hour=19, minute=58)
-    scheduler.add_job(distribution, args=(sql_session,),
-                      trigger='cron', day_of_week='0-6', hour=10, minute=00)
+    # scheduler.add_job(newsletter_about_the_meeting, args=(sql_session,),
+    #                   trigger='cron', day_of_week='0-6', hour=10, minute=00)
 
 # ДЛЯ ТЕСТИРОВАНИЯ РАССЫЛКИ НА ПТН НУЖНО РАЗКОММЕНТИРОВАТЬ СТРОКИ 48-49, РАССЫЛКА БУДЕТ ПРОИСХОДИТЬ ПРИ ЗАПУСКЕ БОТА
     # scheduler.add_job(meeting_reminder_mailing, args=(
     #     sql_session,), next_run_time=datetime.now())
     # scheduler.add_job(meeting_reminder_mailing, args=(sql_session,), trigger='cron',
     #                   day_of_week='thu', hour=19, minute=36)
-    scheduler.add_job(meeting_reminder_mailing, args=(sql_session,), trigger='cron',
-                      day_of_week='0-6', hour=12, minute=00)
+    # scheduler.add_job(meeting_reminder_mailing, args=(sql_session,), trigger='cron',
+    #                   day_of_week='0-6', hour=12, minute=00)
     scheduler.start()
     await bot.set_my_commands(COMMANDS)
     await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
